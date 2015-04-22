b83eabdb0d795b75f3b6ed8c6b43713a76cb0593
dineshj3d <dinesh.mistry@j3dsolutions.com>
2015-04-22 11:23:10 -0700

    Commit #2 arg change

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,11 +1,15 @@
 cacheSolve <- function(x=matrix(), ...) {
+        ##
         m<-x$getmatrix()
         if(!is.null(m)){
                 message("getting cached data")
                 return(m)
         }
+        ##
         matrix<-x$get()
+        ##
         m<-solve(matrix, ...)
+        ##
         x$setmatrix(m)
         m
 }
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,12 +1,18 @@
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x_arg1 = matrix()) {
+        ##
         m<-NULL
+        ##
         set<-function(y){
-                x<<-y
+                x_arg1<<-y
                 m<<-NULL
         }
-        get<-function() x
+        ##
+        get<-function() x_arg1
+        ##
         setmatrix<-function(solve) m<<- solve
+        ##
         getmatrix<-function() m
+        ##
         list(set=set, get=get,
              setmatrix=setmatrix,
              getmatrix=getmatrix)
